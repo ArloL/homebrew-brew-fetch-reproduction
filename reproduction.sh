67f705daec3fcdf3177e52824f7ca206464c1362
Arlo O'Keeffe <tiger@k5d.de>
2025-08-22 20:12:12 +0200

    set homebrew developer

--- reproduction.sh
+++ reproduction.sh
@@ -9,13 +9,10 @@ rm -rf issue-20533-repro
 git clone https://github.com/Homebrew/homebrew-cask issue-20533-repro/homebrew-cask
 ln -s "${PWD}/issue-20533-repro" "$(brew --repository)/Library/Taps/issue-20533-repro"
 ls -lsa "$(brew --repository)/Library/Taps/issue-20533-repro"
-brew desc issue-20533-repro/cask/keepingyouawake
-brew fetch issue-20533-repro/cask/keepingyouawake || true
 brew desc issue-20533-repro/cask/0-ad
 brew fetch issue-20533-repro/cask/0-ad || true
+HOMEBREW_DEVELOPER=yes brew fetch issue-20533-repro/cask/0-ad || true
 
 git clone https://github.com/Homebrew/homebrew-cask "$(brew --repository)/Library/Taps/issue-20533-repro2/homebrew-cask"
-brew desc issue-20533-repro2/cask/keepingyouawake
-brew fetch issue-20533-repro2/cask/keepingyouawake || true
 brew desc issue-20533-repro2/cask/0-ad
 brew fetch issue-20533-repro2/cask/0-ad || true
