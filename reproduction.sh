d9c249f84195230d4e31799052c016b286895db5
Arlo O'Keeffe <tiger@k5d.de>
2025-08-22 15:10:12 +0200

    a

--- reproduction.sh
+++ reproduction.sh
@@ -5,29 +5,28 @@ set -o xtrace
 
 rm -rf "$(brew --repository)/Library/Taps/issue-20533-repro"
 rm -rf issue-20533-repro
-git clone https://github.com/iilonmasc/homebrew-powerlevel9k.git issue-20533-repro/homebrew-powerlevel9k
+mkdir issue-20533-repro
 ln -s "${PWD}/issue-20533-repro" "$(brew --repository)/Library/Taps/issue-20533-repro"
-ls -lsa "$(brew --repository)/Library/Taps/issue-20533-repro/homebrew-powerlevel9k"
-brew desc --debug --verbose issue-20533-repro/powerlevel9k/powerlevel9k
-brew info --debug --verbose issue-20533-repro/powerlevel9k/powerlevel9k
-brew fetch --debug --verbose issue-20533-repro/powerlevel9k/powerlevel9k
-
-ls -lsa "$(brew --repository)/Library/Taps/arlol/homebrew-brew-fetch-reproduction"
-brew desc --debug --verbose arlol/brew-fetch-reproduction/powerlevel9k
-brew info --debug --verbose arlol/brew-fetch-reproduction/powerlevel9k
-brew fetch --debug --verbose arlol/brew-fetch-reproduction/powerlevel9k
 
 git clone https://github.com/Homebrew/homebrew-cask issue-20533-repro/homebrew-cask
 ls -lsa "$(brew --repository)/Library/Taps/issue-20533-repro/homebrew-cask"
 brew desc --debug --verbose issue-20533-repro/cask/0-ad
 brew info --debug --verbose issue-20533-repro/cask/0-ad
-brew fetch --debug --verbose issue-20533-repro/cask/0-ad
+brew fetch --debug --verbose issue-20533-repro/cask/0-ad || true
 
-rm -rf "$(brew --repository)/Library/Taps/arlol"
-rm -rf arlol
-git clone https://github.com/arlol/homebrew-tap.git arlol/homebrew-tap
-ln -s "${PWD}/arlol" "$(brew --repository)/Library/Taps/arlol"
-ls -lsa "$(brew --repository)/Library/Taps/arlol/homebrew-tap"
-brew desc --debug --verbose arlol/tap/chorito
-brew info --debug --verbose arlol/tap/chorito
-brew fetch --debug --verbose arlol/tap/chorito
+git clone https://github.com/arlol/homebrew-tap.git issue-20533-repro/homebrew-arlol-tap
+ls -lsa "$(brew --repository)/Library/Taps/issue-20533-repro/homebrew-arlol-tap"
+brew desc --debug --verbose issue-20533-repro/arlol-tap/chorito
+brew info --debug --verbose issue-20533-repro/arlol-tap/chorito
+brew fetch --debug --verbose issue-20533-repro/arlol-tap/chorito || true
+
+git clone https://github.com/iilonmasc/homebrew-powerlevel9k.git issue-20533-repro/homebrew-powerlevel9k
+ls -lsa "$(brew --repository)/Library/Taps/issue-20533-repro/homebrew-powerlevel9k"
+brew desc --debug --verbose issue-20533-repro/powerlevel9k/powerlevel9k
+brew info --debug --verbose issue-20533-repro/powerlevel9k/powerlevel9k
+brew fetch --debug --verbose issue-20533-repro/powerlevel9k/powerlevel9k || true
+
+ls -lsa "$(brew --repository)/Library/Taps/arlol/homebrew-brew-fetch-reproduction"
+brew desc --debug --verbose arlol/brew-fetch-reproduction/powerlevel9k
+brew info --debug --verbose arlol/brew-fetch-reproduction/powerlevel9k
+brew fetch --debug --verbose arlol/brew-fetch-reproduction/powerlevel9k || true
