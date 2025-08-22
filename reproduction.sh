a5e19c8bd3e3b976730303ff32fc712897c0fed6
Arlo O'Keeffe <tiger@k5d.de>
2025-08-22 14:51:02 +0200

    correct

--- reproduction.sh
+++ reproduction.sh
@@ -8,7 +8,7 @@ rm -rf issue-20533-repro
 git clone https://github.com/iilonmasc/homebrew-powerlevel9k.git issue-20533-repro/homebrew-powerlevel9k
 ln -s "${PWD}/issue-20533-repro" "$(brew --repository)/Library/Taps/issue-20533-repro"
 ls -lsa "$(brew --repository)/Library/Taps/issue-20533-repro/homebrew-powerlevel9k"
-ls -lsa "$(brew --repository)/Library/Taps/homebrew-brew-fetch-reproduction"
+ls -lsa "$(brew --repository)/Library/Taps/arlol/homebrew-brew-fetch-reproduction"
 brew desc --debug --verbose issue-20533-repro/powerlevel9k/powerlevel9k
 brew info --debug --verbose issue-20533-repro/powerlevel9k/powerlevel9k
 brew fetch --debug --verbose issue-20533-repro/powerlevel9k/powerlevel9k
