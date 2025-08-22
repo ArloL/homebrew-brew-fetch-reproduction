f2ec2984122737bd745b71b919cbd271120392e5
Arlo O'Keeffe <tiger@k5d.de>
2025-08-22 14:47:05 +0200

    a

--- reproduction.sh
+++ reproduction.sh
@@ -4,10 +4,9 @@ set -o errexit
 set -o xtrace
 
 rm -rf "$(brew --repository)/Library/Taps/issue-20533-repro"
-rm -rf homebrew-powerlevel9k
-git clone https://github.com/iilonmasc/homebrew-powerlevel9k.git
-mkdir -p "$(brew --repository)/Library/Taps/issue-20533-repro"
-ln -s "${PWD}/homebrew-powerlevel9k" "$(brew --repository)/Library/Taps/issue-20533-repro/homebrew-powerlevel9k"
+rm -rf issue-20533-repro
+git clone https://github.com/iilonmasc/homebrew-powerlevel9k.git issue-20533-repro/homebrew-powerlevel9k
+ln -s "${PWD}/issue-20533-repro" "$(brew --repository)/Library/Taps/issue-20533-repro"
 ls -lsa "$(brew --repository)/Library/Taps/issue-20533-repro/homebrew-powerlevel9k"
 brew desc --debug --verbose issue-20533-repro/powerlevel9k/powerlevel9k
 brew info --debug --verbose issue-20533-repro/powerlevel9k/powerlevel9k
